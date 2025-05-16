27cc7099bd1686da20b6b31f881bfa429870feac
Denis Borges <denisborges@protechsolutions.com.br>
2025-05-16 12:01:43 -0300

    Adicionar arquivos de projeto.

---
+++ src/Sig.SecurityService.Tenant/Sig.SecurityService.Tenant.Common/Results/CustomErrors/CustomError.cs
@@ -0,0 +1,39 @@
+﻿using FluentResults;
+using System.Text.Json;
+
+namespace Sig.SecurityService.Tenant.Common.Results.CustomErrors;
+
+public abstract class CustomError : Error
+{
+    private readonly string _code;
+    private readonly string _message;
+    private readonly string _source;
+
+    protected CustomError(Type sourceType, string code, string message)
+        : base(message)
+    {
+        _code = code;
+        _message = message;
+        _source = sourceType.Name;
+
+        Metadata[nameof(_code)] = code;
+        Metadata[nameof(_message)] = message;
+        Metadata[nameof(_source)] = _source;
+    }
+
+    protected abstract object BuildToObject();
+
+    public virtual object ToObject()
+    {
+        return new
+        {
+            Type = GetType().Name,
+            ErrorDetail = BuildToObject(),
+            Metadata = Metadata.ToDictionary(kvp => kvp.Key, kvp => kvp.Value)
+        };
+    }
+
+    public override string ToString() =>
+        JsonSerializer.Serialize(ToObject(), new JsonSerializerOptions { WriteIndented = true });
+}
+
---
+++ src/Sig.SecurityService.Tenant/Sig.SecurityService.Tenant.Common/Results/CustomErrors/ValidationError.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation.Results;
+
+namespace Sig.SecurityService.Tenant.Common.Results.CustomErrors;
+
+public class ValidationError : CustomError
+{
+    private readonly IReadOnlyCollection<string> _errors;
+
+    public ValidationError(IEnumerable<string> errors)
+        : base(typeof(ValidationError), "1", "One or more validation errors occurred.")
+    {
+        _errors = errors.ToList().AsReadOnly();
+        foreach (var error in _errors)
+            Metadata.TryAdd($"ValidationError-{Guid.NewGuid()}", error);
+    }
+
+    public ValidationError(ValidationResult validationResult)
+        : this(validationResult.Errors.Select(FormatValidationFailure))
+    {
+        foreach (var failure in validationResult.Errors)
+            Metadata.TryAdd($"Property-{failure.PropertyName}", failure.ErrorMessage);
+    }
+
+    protected override object BuildToObject()
+    {
+        return new
+        {
+            Type = nameof(ValidationError),
+            Code = "1",
+            Message = "One or more validation errors occurred.",
+            Errors = _errors
+        };
+    }
+
+    private static string FormatValidationFailure(ValidationFailure failure)
+        => $"{failure.PropertyName}: {failure.ErrorMessage}";
+}
+
